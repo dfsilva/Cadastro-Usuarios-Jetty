33731593b8919f6b0f8be71ed3f50345ade55966
diego <diegosiuniube@gmail.com>
2018-04-23 23:19:25 -0300

    fixes

--- src/main/java/br/com/anhanguera/cadastro/usuario/Principal.java
+++ src/main/java/br/com/anhanguera/cadastro/usuario/Principal.java
@@ -44,101 +44,14 @@ public class Principal {
 				org.glassfish.jersey.servlet.ServletContainer.class, "/api/*");
 		jerseyServlet.setInitOrder(0);
 
-		// Tells the Jersey Servlet which REST service/class to load.
 		jerseyServlet.setInitParameter(
 				"jersey.config.server.provider.classnames",
 				UsuarioController.class.getCanonicalName());
 
-
-//		servletHandler.addServlet(
-//				new ServletHolder(new CadastroUsuarioServlet()),
-//				"/usuarios");
-//
 		servidor.setHandler(servletHandler);
 				
 		servidor.start();
 		servidor.join();
 
 	}
-	
-	
-	static class CadastroUsuarioServlet extends HttpServlet{
-		
-			@Override
-			protected void doGet(HttpServletRequest req, 
-					HttpServletResponse resp) 
-							throws ServletException, 
-							IOException {
-				resp.setContentType("application/json");
-				resp.setStatus(HttpServletResponse.SC_OK);
-				
-				resp.getWriter()
-					.println(new Gson().toJson(UsuarioDAO.listar()));
-			}
-			
-			@Override
-			protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-				Long idExcluir = Long.parseLong(req.getParameter("id")) ;
-				UsuarioDAO.excluir(idExcluir);
-				resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
-			}
-			
-			private Usuario recuperarusuario(HttpServletRequest req){
-				StringBuffer jsonBuffer = new StringBuffer();
-				String linha = null;
-				try{
-					BufferedReader reader = req.getReader();
-					while((linha = reader.readLine()) != null){
-						jsonBuffer.append(linha);
-					}
-				}catch (Exception e) {
-					e.printStackTrace();
-				}
-				
-				Usuario usuario = new Gson()
-						.fromJson(jsonBuffer.toString(), Usuario.class);
-				
-				return usuario;
-			}
-			
-			
-			@Override
-			protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-			
-				Usuario usuario = recuperarusuario(req);
-				
-				UsuarioDAO.atualizar(usuario);
-				
-				resp.setContentType("application/json");
-				resp.setStatus(HttpServletResponse.SC_OK);
-				
-				resp.getWriter()
-					.println(new Gson().toJson(usuario));
-				
-			}
-			
-			@Override
-			protected void doPost(HttpServletRequest req, 
-					HttpServletResponse resp) 
-							throws ServletException, IOException {
-			
-				Usuario usuario = recuperarusuario(req);	
-				
-				try {
-					Usuario usuarioInserido = UsuarioDAO.inserir(usuario);
-					resp.setContentType("application/json");
-					resp.setStatus(HttpServletResponse.SC_CREATED);
-					
-					resp.getWriter()
-						.println(new Gson().toJson(usuarioInserido));
-					
-				} catch (SQLException e) {
-					e.printStackTrace();
-				} catch (PropertyVetoException e) {
-					e.printStackTrace();
-				}
-			}
-		
-	}
-	
 }
